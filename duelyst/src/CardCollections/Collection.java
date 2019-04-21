70141e5f1cd9b2cee85c728402fc0f3b2b80f604
pooya <pooyamoini2000@gmail.com>
2019-04-22 00:16:34 +0430

    no specific change

--- duelyst/src/CardCollections/Collection.java
+++ duelyst/src/CardCollections/Collection.java
@@ -1,10 +1,10 @@
 package CardCollections;
+
 import effects.*;
+
 import java.util.ArrayList;
 
 import effects.Cards;
-
-import java.util.ArrayList;
 
 public class Collection {
     private ArrayList<Cards> cards;
@@ -21,58 +21,41 @@ public class Collection {
     }
 
 
-    public String search(String name){
-        if(findCardByName(name)!=null){
-            return findCardByName(name).getId();
-        }else if(findItemByName(name)!=null){
-            return findItemByName(name).getId();
-        }else{
-            return null;
-        }
+    public String search(String name) {
+        if (findCard(name) != null)
+            return findCard(name).getId();
+
+        else if (findItem(name) != null)
+            return findItem(name).getId();
+        return null;
     }
 
-    private Cards findCardByName(String cardName){
-        for(int i=0;i<cards.size();i++){
-            if(cards.get(i).getName().equals(cardName)){
-                return cards.get(i);
-            }
+    private Cards findCard(String cardNameID) {
+        for (Cards card : cards) {
+            if (card.getName().equals(cardNameID))
+                return card;
 
+            if (card.getId().equals(cardNameID))
+                return card;
         }
         return null;
     }
 
-    private Item findItemByName(String itemName){
-        for(int i=0;i<items.size();i++){
-            if(items.get(i).getName().equals(itemName)){
-                return items.get(i);
-            }
+    private Item findItem(String itemNameID) {
+        for (Item item : items) {
+            if (item.getName().equals(itemNameID))
+                return item;
 
+            if (item.getId().equals(itemNameID))
+                return item;
         }
         return null;
     }
 
-    private Cards findCardByID(String cardID){
-        for(int i=0;i<cards.size();i++){
-            if(cards.get(i).getId().equals(cardID)){
-                return cards.get(i);
-            }
-        }
-        return null;
-    }
-
-    private Item findItemByID(String itemID){
-        for(int i=0;i<items.size();i++){
-            if(items.get(i).getId().equals(itemID)){
-                return items.get(i);
-            }
-        }
-        return null;
-    }
-
-    public String createDeck(String deckName){
-        Deck deck=new Deck(deckName);
-        for(Deck deck1:decks){
-            if(deck1.getName().equals(deckName)){
+    public String createDeck(String deckName) {
+        Deck deck = new Deck(deckName);
+        for (Deck deck1 : decks) {
+            if (deck1.getName().equals(deckName)) {
                 return "Deck Already Exist! Please Try again with another DeckName.";
             }
         }
@@ -80,59 +63,51 @@ public class Collection {
         return "Deck Successfully created";
     }
 
-    public Deck findDeck(String deckName){
-        for (Deck deck:decks) {
-            if(deck.getName().equals(deckName)){
+    public Deck findDeck(String deckName) {
+        for (Deck deck : decks) {
+            if (deck.getName().equals(deckName)) {
                 return deck;
             }
         }
         return null;
     }
 
-    public void deleteDeck(String deckName){
-        Deck garbageDeck=findDeck(deckName);
-        if(garbageDeck!=null){
+    public void deleteDeck(String deckName) {
+        Deck garbageDeck = findDeck(deckName);
+        if (garbageDeck != null) {
             decks.remove(garbageDeck);
-            return;
         }
-        return;
     }
 
-    public boolean isCardInDeck(String cardID,String deckName){
-        if(findDeck(deckName)==null || findCardByID(cardID)==null){
+    public boolean isCardInDeck(String cardID, String deckName) {
+        if (findDeck(deckName) == null || findCard(cardID) == null) {
             return false;
         }
-        if(findDeck(deckName).getCards().contains(findCardByID(cardID))){
-            return true;
-        }
-        return false;
+        return findDeck(deckName).getCards().contains(findCard(cardID));
     }
 
-    public boolean isItemInDeck(String itemID,String deckName){
-        if(findDeck(deckName)==null || findItemByID(itemID)==null){
+    public boolean isItemInDeck(String itemID, String deckName) {
+        if (findDeck(deckName) == null || findItem(itemID) == null) {
             return false;
         }
-        if(findDeck(deckName).getItem().getName().equals(findItemByID(itemID).getName())){
-            return true;
-        }
-        return false;
+        return findDeck(deckName).getItem().getName().equals(findItem(itemID).getName());
     }
 
-    public String addToDeck(String ID,String deckName){
-        if(findCardByID(ID)==null || findItemByID(ID)==null){
+    public String addToDeck(String ID, String deckName) {
+        if (findCard(ID) == null || findItem(ID) == null)
             return "No such Card/Item ID found in collection";
-        }
-        if(!isCardInDeck(ID, deckName) || !isItemInDeck(ID, deckName)){
+
+        if (!isCardInDeck(ID, deckName) || !isItemInDeck(ID, deckName))
             return "This Card/Item ID doesn't exist in this deck!";
-        }
-        if(findDeck(deckName).getNumberOfDeckCards()+1>20){
+
+        if (findDeck(deckName).getNumberOfDeckCards() + 1 > 20)
             return "The Deck card storage is full.";
-        }
-        if(findDeck(deckName).getHero()!=null ){
+
+        if (findDeck(deckName).getHero() != null)
             return "This deck has hero";
-        }else{
-            findDeck(deckName).setHero((Hero) findCardByID(ID));
-            return "Hero added succesfully";
+        else {
+            findDeck(deckName).setHero((Hero) findCard(ID));
+            return "Hero added successfully";
         }
 //        if(findCardByID(ID)!=null){
 //            findDeck(deckName).getCards().add(findCardByID(ID));
