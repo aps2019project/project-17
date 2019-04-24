874de1927921c2800d12f3dcd33a66acd0a76814
Ali <younesi_ali@yahoo.com>
2019-04-24 20:19:13 +0430

    buff structure completely created (exp: action function) some new class created (all of them related to buff)

--- duelyst/src/effects/Minion.java
+++ duelyst/src/effects/Minion.java
@@ -4,15 +4,40 @@ public class Minion {
     private Enum type;
     private Spell specialPower;
     private Enum classType;
-    private int actionPower;
+    private int attackPower;
     private int healthPoint;
     private int manaPoint;
     private String desc;
     private int attackRange;
+    private boolean isHolyBuffActive;
+    private boolean canMove;
+    private boolean canCounterAttack;
 
     public void attack(String id) {
     }
 
     public void move(int x, int y) {
+    }
+
+    public void activeHolyBuff() {
+        isHolyBuffActive = true;
+    }
+
+    public void setCanMove(boolean canMove) {
+        this.canMove = canMove;
+    }
+
+    public void setCanCounterAttack(boolean canCounterAttack) {
+        this.canCounterAttack = canCounterAttack;
+    }
+
+    public void changeHealth(int changingValue)
+    {
+        this.healthPoint += changingValue;
+    }
+
+    public void changeAttackPower(int changingValue)
+    {
+        this.attackPower += changingValue;
     }
 }
---
+++ duelyst/src/effects/TargetType.java
@@ -0,0 +1,10 @@
+package effects;
+
+public enum TargetType {
+    ENEMY,
+    INSIDER,
+    ENEMY_HERO,
+    INSIDER_HERO,
+    CELL,
+    NONE
+}
