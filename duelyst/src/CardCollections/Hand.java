91a53bb7d9f3420e50348025dd8cf8ac049d6c8b
Ali <younesi_ali@yahoo.com>
2019-04-24 20:24:49 +0430

    buff structure completely created (exp: action function) some new class created (all of them related to buff) Cards change to Card

--- duelyst/src/CardCollections/Hand.java
+++ duelyst/src/CardCollections/Hand.java
@@ -1,21 +1,21 @@
 package CardCollections;
 
-import effects.Cards;
+import effects.Card;
 
 import java.util.ArrayList;
 
 public class Hand {
-    private ArrayList<Cards> cards;
+    private ArrayList<Card> cards;
 
-    public ArrayList<Cards> getCards() {
+    public ArrayList<Card> getCards() {
         return cards;
     }
 
-    public void addCard(Cards card) {
+    public void addCard(Card card) {
         cards.add(card);
     }
 
-    public void removeCard(Cards card) {
+    public void removeCard(Card card) {
         for (int i = 0; i < cards.size(); i++) {
             if (cards.get(i).getName().equals(card.getName())) {
                 cards.remove(i);
