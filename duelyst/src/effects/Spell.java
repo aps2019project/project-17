2a97628b4a85ce1e8497ec771ed27cb74388d3bf
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2019-04-25 15:44:33 +0430

    fixing a little error,deleting wring Item class,created new View package

--- duelyst/src/effects/Spell.java
+++ duelyst/src/effects/Spell.java
@@ -6,4 +6,8 @@ public class Spell extends Card {
     private Buff buff;
     private TargetRange target;
     private TargetType targetType;
+
+    public Spell(String name, String id, int price) {
+        super(name, id, price);
+    }
 }
