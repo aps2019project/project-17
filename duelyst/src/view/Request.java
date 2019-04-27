76ccc50b81f6613f864bad08f0663d2e93de603b
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2019-04-27 14:48:36 +0430

    added methods to request class

--- duelyst/src/view/Request.java
+++ duelyst/src/view/Request.java
@@ -1,4 +1,21 @@
 package view;
 
+import java.util.Scanner;
+
 public class Request {
+    protected Scanner scanner = new Scanner(System.in);
+    protected String command;
+    protected ErrorType error = null;
+
+    public void getNewCommand() {
+        command = scanner.nextLine();
+    }
+
+    public ErrorType getError() {
+        return error;
+    }
+
+    public void setError(ErrorType error) {
+        this.error = error;
+    }
 }
