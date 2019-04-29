fb12692142fa682dfaf56043cd9a8e0f6ab97f35
Ali <younesi_ali@yahoo.com>
2019-04-29 20:27:58 +0430

    Hero Constructor created

--- duelyst/src/effects/Hero.java
+++ duelyst/src/effects/Hero.java
@@ -1,32 +1,15 @@
 package effects;
 
 public class Hero extends Minion {
+    private int coolDown;
 
-    private int attackPower;
-    private int healthPoint;
-    private Spell specialPower;
-
-    public Hero(String name, String id, int price, int attackPower, int healthPoint, int manaPoint, int attackRange, int distanceCanMove, int maxRangeToInput, int attackPower1, int healthPoint1, Spell specialPower) {
-        super(name, id, price, attackPower, healthPoint, manaPoint, attackRange, distanceCanMove, maxRangeToInput);
-        this.attackPower = attackPower1;
-        this.healthPoint = healthPoint1;
-        this.specialPower = specialPower;
+    public Hero(String name, String id, int price, int manaPoint, int healthPoint, int attackPower, MinionType minionType, int attackRange, int distanceCanMove, int maxRangeToInput, AttackType attackType, int coolDown) {
+        super(name, id, price, manaPoint, healthPoint, attackPower, minionType, attackRange, distanceCanMove, maxRangeToInput, attackType);
+        this.coolDown = coolDown;
     }
 
     public void show() {
         System.out.println("Name : " + this.name + " - AP : " + this.attackPower + " - HP : " + this.healthPoint + " - Class : " + this.minionType + " - Special power : " + this.specialPower);
         //todo needs to have toString for minionType and specialPower and if they don't have nothing must be printed
-    }
-
-    public int getAttackPower() {
-        return attackPower;
-    }
-
-    public int getHealthPoint() {
-        return healthPoint;
-    }
-
-    public Spell getSpecialPower() {
-        return specialPower;
     }
 }
