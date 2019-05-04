e7c784c69d83399e70c907163a1ce5e01905bc83
Ali <younesi_ali@yahoo.com>
2019-05-04 14:44:37 +0430

    desc removed from spell

--- duelyst/src/effects/Spell.java
+++ duelyst/src/effects/Spell.java
@@ -1,20 +1,20 @@
 package effects;
+
 public class Spell extends Card {
     private int manaPoint;
-    private Buff buff;;
+    private Buff buff;
 
-    public Spell(String name, String id, int price, int manaPoint, String desc) {
+    public Spell(String name, String id, int price, int manaPoint) {
         super(name, id, price);
-        this.desc = desc;
         this.manaPoint = manaPoint;
     }
-    public void addBuff(BuffDetail buffDetail)
-    {
+
+    public void addBuff(BuffDetail buffDetail) {
         this.buff.addBuff(buffDetail);
     }
 
-    public void show(){
-        System.out.println("Name : "+this.name+" - MP : "+this.manaPoint+" - Desc : "+this.desc);
+    public void show() {
+        System.out.println("Name : " + this.name + " - MP : " + this.manaPoint + " - Desc : " + this.desc);
     }
 
     public int getManaPoint() {
@@ -23,16 +23,6 @@ public class Spell extends Card {
 
     public void setManaPoint(int manaPoint) {
         this.manaPoint = manaPoint;
-    }
-
-    @Override
-    public String getDesc() {
-        return desc;
-    }
-
-    @Override
-    public void setDesc(String desc) {
-        this.desc = desc;
     }
 
     public Buff getBuff() {
