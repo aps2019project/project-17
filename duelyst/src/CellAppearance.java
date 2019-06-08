bf04106617bb36c0e788afaeb1f147322ed2d254
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2019-06-08 20:16:17 +0430

    cellAppearance added

---
+++ duelyst/src/CellAppearance.java
@@ -0,0 +1,47 @@
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class CellAppearance {
+
+    private Rectangle cell;
+
+    public CellAppearance() {
+        cell=new Rectangle(Main.WIDTH_OF_WINDOW / 22, Main.HEIGHT_OF_WINDOW/12);
+        cell.setArcWidth(20);
+        cell.setArcHeight(20);
+        dark();
+        fill();
+    }
+
+    private void changeLight(double value) {
+        cell.setOpacity(value);
+    }
+
+    private void fill() {
+        cell.setFill(Color.rgb( 117, 46, 214 ));
+    }
+
+    public void light() {// TODO: 6/8/2019
+        changeLight(0.7);
+        cell.setFill(Color.rgb( 214, 46, 102 ));
+    }
+
+    public void dark() {
+        changeLight(0.3);
+        cell.setFill(Color.rgb( 117, 46, 214 ));
+    }
+
+    public void add(Group group) {// TODO: 6/8/2019
+        group.getChildren().addAll(cell);
+    }
+
+    public Rectangle getCell() {
+        return cell;
+    }
+
+    public void handleEvents(){
+        cell.setOnMouseEntered(event -> light());
+        cell.setOnMouseExited(event -> dark());
+    }
+}
