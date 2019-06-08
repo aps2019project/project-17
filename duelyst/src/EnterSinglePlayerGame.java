0e74405da0f97d75f1df31463b622f9772a9c661
pooya <pooyamoini2000@gmail.com>
2019-06-08 21:58:34 +0430

    menu of Battle is going to be implemented

--- duelyst/src/EnterSinglePlayerGame.java
+++ duelyst/src/EnterSinglePlayerGame.java
@@ -1,6 +1,4 @@
-import Appearance.ColorAppearance;
 import Appearance.FontAppearance;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -15,23 +13,22 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class StartingBattleAppearance {
+public class EnterSinglePlayerGame {
     private static Stage firstWindow = new Stage();
-    private static Text singlePlayer = new Text("Single Player");
-    private static Text multiPlayer = new Text("multi Player");
-    private static Text modeChoose = new Text("select which type of battle you want to play");
+    private static Text customMode = new Text("Custom Mode");
+    private static Text storyMode = new Text("Story Mode");
+    private static Text modeChoose = new Text("select which type of Single Play you want to play");
     private static VBox root = new VBox();
     private static Scene scene = new Scene(root);
-    private static Rectangle rectangle = new Rectangle(210, 60);
-    private static Rectangle rectangle1 = new Rectangle(210, 60);
 
     static {
         try {
-            ;
+            Rectangle rectangle = new Rectangle(210, 60);
+            Rectangle rectangle1 = new Rectangle(210, 60);
             rectangle.setFill(new ImagePattern(new Image(new FileInputStream("button.png"))));
             rectangle1.setFill(new ImagePattern(new Image(new FileInputStream("button.png"))));
-            StackPane stackPane1 = new StackPane(rectangle, singlePlayer);
-            StackPane stackPane2 = new StackPane(rectangle1, multiPlayer);
+            StackPane stackPane1 = new StackPane(rectangle, customMode);
+            StackPane stackPane2 = new StackPane(rectangle1, storyMode);
             HBox hBox = new HBox(stackPane1, stackPane2);
             hBox.setSpacing(100);
             root.setSpacing(65);
@@ -47,32 +44,33 @@ public class StartingBattleAppearance {
         firstWindow.initModality(Modality.APPLICATION_MODAL);
         firstWindow.setMinWidth(Main.WIDTH_OF_WINDOW / 2.5);
         firstWindow.setMinHeight(Main.HEIGHT_OF_WINDOW / 2.5);
-        singlePlayer.setFont(FontAppearance.FONT_BUTTON);
-        multiPlayer.setFont(FontAppearance.FONT_BUTTON);
+        customMode.setFont(FontAppearance.FONT_BUTTON);
+        storyMode.setFont(FontAppearance.FONT_BUTTON);
         modeChoose.setFont(FontAppearance.FONT_BUTTON);
-        singlePlayer.setFill(Color.WHITE);
-        multiPlayer.setFill(Color.WHITE);
+        customMode.setFill(Color.WHITE);
+        storyMode.setFill(Color.WHITE);
         firstWindow.setScene(scene);
     }
 
     static {
-        BackgroundFill bgFill = new BackgroundFill(ColorAppearance.COLOR_TITLES_OF_SHOP, CornerRadii.EMPTY, Insets.EMPTY);
-        root.setBackground(new Background(bgFill));
+        try {
+            Image image = new Image(new FileInputStream("background0.jpg"));
+            BackgroundImage bgImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+            Background background = new Background(bgImage);
+            root.setBackground(background);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     static {
-        singlePlayer.setOnMouseClicked(e -> {
-            EnterSinglePlayerGame.disPlay();
-            firstWindow.close();
-        });
-        rectangle.setOnMouseClicked(e -> {
-            EnterSinglePlayerGame.disPlay();
+        storyMode.setOnMouseClicked(e -> {
+            SelectModeStorySingle.disPlay();
             firstWindow.close();
         });
     }
 
     public static void disPlay() {
         firstWindow.showAndWait();
-
     }
 }
--- duelyst/src/Main.java
+++ duelyst/src/Main.java
@@ -153,6 +153,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+//        GameController.main();
         launch(args);
     }
 
